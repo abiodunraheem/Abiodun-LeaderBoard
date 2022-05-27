ea61941db66baefa1c4f4206c1c5ea294bbd9fb3
abiodunraheem <raheemabiodun23@gmail.com>
2022-05-27 11:36:00 +0100

    Add deployment page

--- src/modules/fromAPI.js
+++
@@ -1,8 +0,0 @@
-const retrieveFromAPI = async (baseURL) => {
-  const res = await fetch(baseURL);
-  const lead = await res.json();
-  const leaders = lead.result;
-  return leaders;
-};
-
-export default retrieveFromAPI;
